d225ffc2cd35b5091c220150f000903dd77e3b3a
Raphaël Coutu <raph244@gmail.com>
2024-12-20 07:28:15 -0500

    wip

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"slices"
 	"strings"
 	"unicode"
 
@@ -16,9 +17,11 @@ import (
 )
 
 type Dosage struct {
+	Id          int    `json:"id"`
 	Text        string `json:"text"`
 	Dose        string `json:"dose"`
 	DoseUnit    string `json:"dose_unit"`
+	Route       string `json:"route"`
 	FrequencyId int    `json:"frequency_id"`
 	Frequency   string `json:"frequency"`
 }
@@ -32,6 +35,7 @@ func main() {
 	defer file.Close()
 
 	var dosages []Dosage
+	i := 1
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		oLine := scanner.Text()
@@ -41,12 +45,11 @@ func main() {
 			log.Fatal(err)
 		}
 
+		dosage.Id = i
+
 		dosages = append(dosages, dosage)
 
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
+		i++
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -103,6 +106,7 @@ func MapAll(line string) (Dosage, error) {
 	}
 
 	dosage.Dose, dosage.DoseUnit = MapDose(line)
+	dosage.Route = MapRoute(line, dosage)
 
 	dosage.FrequencyId, dosage.Frequency = MapFrequency(line)
 
@@ -110,6 +114,10 @@ func MapAll(line string) (Dosage, error) {
 }
 
 func MapDose(line string) (string, string) {
+
+	if isComplexDosage(line) {
+		return "", ""
+	}
 
 	line = RemoveFraction(line)
 
@@ -129,9 +137,23 @@ func MapDose(line string) (string, string) {
 		return dose, "capsule"
 	}
 
+	if regexp.MustCompile(`(LA|UNE) CAPSULE`).MatchString(line) {
+		return "1", "capsule"
+	}
+
 	re = regexp.MustCompile(`([0-9]+) (VAPORISATIONS|VAPORISATION)`)
 	if re.MatchString(line) {
 		return re.FindStringSubmatch(line)[1], "vaporisation"
+	}
+
+	re = regexp.MustCompile(`VAPORISE(?:R|Z) ([0-9]+) FOIS`)
+	if re.MatchString(line) {
+		return re.FindStringSubmatch(line)[1], "vaporisation"
+	}
+
+	re = regexp.MustCompile(`(VAPORISER|VAPORISEZ)`)
+	if re.MatchString(line) {
+		return "1", "vaporisation"
 	}
 
 	re = regexp.MustCompile(`([0-9]+) (INHALATIONS|INHALATION)`)
@@ -139,23 +161,104 @@ func MapDose(line string) (string, string) {
 		return re.FindStringSubmatch(line)[1], "bouffée"
 	}
 
-	re = regexp.MustCompile(`([0-9]+) (GRAMMES|GRAMME|G)`)
+	re = regexp.MustCompile(`([0-9]+) (GOUTTES?|G )`)
+	if re.MatchString(line) {
+		return re.FindStringSubmatch(line)[1], "goutte"
+	}
+
+	re = regexp.MustCompile(`([0-9]+) (GRAMMES?|G )`)
 	if re.MatchString(line) {
 		return re.FindStringSubmatch(line)[1], "g"
 	}
 
-	re = regexp.MustCompile(`([0-9]+)(GRAMMES|GRAMME|G|G. )`)
+	re = regexp.MustCompile(`([0-9]+)(GRAMMES?|G|G\. )`)
 	if re.MatchString(line) {
 		return re.FindStringSubmatch(line)[1], "g"
 	}
+
+	re = regexp.MustCompile(`([0-9]+|UN) (TIMBRES?|PATCHS?)`)
+	if re.MatchString(line) {
+		if re.FindStringSubmatch(line)[1] == "UN" {
+			return "1", "timbre"
+		}
+		return re.FindStringSubmatch(line)[1], "timbre"
+	}
+
 	return "", ""
+}
+
+func MapRoute(line string, dosage Dosage) string {
+
+	if strings.Contains(line, "CHAQUE NARINE") || strings.Contains(line, "DANS LES NARINES") {
+		return "nasale"
+	}
+
+	if regexp.MustCompile(`INTRA(-|\s)?MUSCULAIRE`).MatchString(line) {
+		return "intramusculaire"
+	}
+
+	if regexp.MustCompile(`SOUS(-|\s)?CUTANEE`).MatchString(line) {
+		return "sous-cutané"
+	}
+
+	if strings.Contains(line, "OEIL") || strings.Contains(line, "ŒIL") || strings.Contains(line, "YEUX") {
+		if strings.Contains(line, "GAUCHE") {
+			return "oeil gauche"
+		} else if strings.Contains(line, "DROIT") {
+			return "oeil droit"
+		} else if strings.Contains(line, "YEUX") {
+			return "dans les 2 yeux"
+		}
+		return "oculaire"
+	}
+
+	if regexp.MustCompile(`OREILLES?`).MatchString(line) {
+		if strings.Contains(line, "GAUCHE") {
+			return "oreille gauche"
+		} else if strings.Contains(line, "DROIT") {
+			return "oreille droit"
+		} else if strings.Contains(line, "OREILLES") {
+			return "dans les 2 oreilles"
+		}
+		return "otique"
+	}
+
+	if regexp.MustCompile(`APPLIQUE(R|Z)|APPLICATION LOCALE`).MatchString(line) {
+		return "topique"
+	}
+
+	if strings.Contains(line, "BOIRE") || regexp.MustCompile(`DISSOU.*\sVERRE D'EAU`).MatchString(line) {
+		return "oral"
+	}
+
+	if strings.Contains(line, "SOUS LA LANGUE") {
+		return "sublingual"
+	}
+
+	if regexp.MustCompile(`INHALE(R|Z) (LE CONTENU D'UNE )?CAPSULE`).MatchString(line) {
+		return "inhalation"
+	}
+
+	if slices.Contains([]string{"comprimé", "capsule"}, dosage.DoseUnit) {
+		return "oral"
+	} else if dosage.DoseUnit == "bouffée" {
+		return "inhalation"
+	} else if dosage.DoseUnit == "timbre" {
+		return "topique"
+	}
+
+	return ""
 }
 
 func MapFrequency(line string) (int, string) {
 	isPrn := false
 	withFood := false
 
-	if strings.Contains(line, "PRN") || strings.Contains(line, "AU BESOIN") || strings.Contains(line, "SI BESOIN") || strings.Contains(line, "AS NEEDED") {
+	if isComplexDosage(line) {
+		return 0, ""
+	}
+
+	if strings.Contains(line, "PRN") || strings.Contains(line, "AU BESOIN") || strings.Contains(line, "SI BESOIN") || strings.Contains(line, "AS NEEDED") || regexp.MustCompile(`SI (DOULEURS?)`).MatchString(line) {
 		isPrn = true
 	}
 
@@ -163,14 +266,14 @@ func MapFrequency(line string) (int, string) {
 		withFood = true
 	}
 
-	// # FOIS PAR JOUR
-	re := regexp.MustCompile(`([0-9]+) FOIS PAR JOUR`)
+	// # FOIS PAR JOUR (FR)
+	re := regexp.MustCompile(`([0-9]+|UNE) FOIS PAR JOUR`)
 	if re.MatchString(line) {
 		freq := re.FindStringSubmatch(line)[1]
 
 		if isPrn {
 			// prn = true
-			if freq == "1" {
+			if freq == "1" || freq == "UNE" {
 				if regexp.MustCompile(`(AU|AVEC LE) DEJEUNER`).MatchString(line) {
 					return 0, "1 fois par jour au déjeuner PRN"
 				} else if regexp.MustCompile(`AVANT LE DEJEUNER`).MatchString(line) {
@@ -181,6 +284,8 @@ func MapFrequency(line string) (int, string) {
 					return 0, "1 fois par jour au dîner PRN"
 				} else if strings.Contains(line, "AU SOUPER") {
 					return 0, "1 fois par jour au souper PRN"
+				} else if regexp.MustCompile(`AVANT (LE )?COUCHER`).MatchString(line) {
+					return 0, "1 fois par jour au coucher PRN"
 				} else if strings.Contains(line, "AU COUCHER") {
 					return 0, "1 fois par jour au coucher PRN"
 				}
@@ -195,7 +300,7 @@ func MapFrequency(line string) (int, string) {
 			}
 		} else {
 			// prn = false
-			if freq == "1" {
+			if freq == "1" || freq == "UNE" {
 				if regexp.MustCompile(`(AU|AVEC LE) DEJEUNER`).MatchString(line) {
 					return 0, "1 fois par jour au déjeuner"
 				} else if regexp.MustCompile(`AVANT LE DEJEUNER`).MatchString(line) {
@@ -206,6 +311,8 @@ func MapFrequency(line string) (int, string) {
 					return 0, "1 fois par jour au dîner"
 				} else if strings.Contains(line, "AU SOUPER") {
 					return 0, "1 fois par jour au souper"
+				} else if regexp.MustCompile(`AVANT (LE )?COUCHER`).MatchString(line) {
+					return 0, "1 fois par jour au coucher"
 				} else if strings.Contains(line, "AU COUCHER") {
 					return 0, "1 fois par jour au coucher"
 				}
@@ -216,7 +323,7 @@ func MapFrequency(line string) (int, string) {
 				if strings.Contains(line, "DEJEUNER") && strings.Contains(line, "SOUPER") {
 					return 0, "2 fois par jour au déjeuner et au souper"
 				} else if strings.Contains(line, "MATIN") && strings.Contains(line, "SOIR") && withFood {
-					return 0, "2 fois par jour au déjeuner et au souper "
+					return 0, "2 fois par jour au déjeuner et au souper"
 				}
 
 				return 0, "2 fois par jour"
@@ -228,14 +335,105 @@ func MapFrequency(line string) (int, string) {
 		}
 	}
 
-	// # FOIS PAR SEMAINE
-	re = regexp.MustCompile(`([0-9]+) FOIS PAR SEMAINE`)
+	// # FOIS PAR JOUR (EN)
+	if strings.Contains(line, "DAILY") {
+		if strings.Contains(line, "TWICE") {
+			//
+		} else if strings.Contains(line, "THREE TIMES") {
+			//
+		} else {
+			if isPrn {
+				return 0, "1 fois par jour PRN"
+			} else {
+				if strings.Contains(line, "BREAKFAST") {
+					return 0, "1 fois par jour au déjeuner"
+				} else if strings.Contains(line, "IN THE MORNING") {
+					return 0, "1 fois par jour le matin"
+				} else if strings.Contains(line, "AT BEDTIME") {
+					return 0, "1 fois par jour au coucher"
+				}
+				return 0, "1 fois par jour"
+			}
+		}
+	}
+
+	// AUX # HEURES
+	re = regexp.MustCompile(`(?:AUX|TOU(?:TE)?S LES) ([0-9]+)( A [0-9]+)?\s+HEURES`)
 	if re.MatchString(line) {
 		freq := re.FindStringSubmatch(line)[1]
 
 		if isPrn {
 			// prn = true
-			if freq == "1" {
+			if freq == "3" {
+				return 0, "q3h PRN"
+			} else if freq == "4" {
+				return 0, "q4h PRN"
+			} else if freq == "6" {
+				return 0, "q6h PRN"
+			} else if freq == "8" {
+				return 0, "q8h PRN"
+			} else if freq == "12" {
+				return 0, "q8h PRN"
+			} else if freq == "24" {
+				return 0, "q8h PRN"
+			}
+		} else {
+			// prn = false
+			if freq == "3" {
+				return 0, "q3h"
+			} else if freq == "4" {
+				return 0, "q4h"
+			} else if freq == "6" {
+				return 0, "q6h"
+			} else if freq == "8" {
+				return 0, "q8h"
+			} else if freq == "12" {
+				return 0, "q8h"
+			} else if freq == "24" {
+				return 0, "q8h"
+			}
+		}
+	}
+
+	// AUX # HEURES
+	re = regexp.MustCompile(`AUX ([0-9]+)( A [0-9]+)?\s+MIN(UTES|UTE)?`)
+	if re.MatchString(line) {
+		freq := re.FindStringSubmatch(line)[1]
+
+		if isPrn {
+			// prn = true
+			if freq == "5" {
+				return 0, "q5min PRN"
+			} else if freq == "10" {
+				return 0, "q10min PRN"
+			} else if freq == "15" {
+				return 0, "q15min PRN"
+			} else if freq == "30" {
+				return 0, "q30min PRN"
+			}
+		} else {
+			// prn = false
+			if freq == "5" {
+				return 0, "q5min"
+			} else if freq == "10" {
+				return 0, "q10min"
+			} else if freq == "15" {
+				return 0, "q15min"
+			} else if freq == "30" {
+				return 0, "q30min"
+			}
+		}
+	}
+
+	// # FOIS PAR SEMAINE
+	re = regexp.MustCompile(`([0-9]+) FOIS PAR SEMAINE|(ONCE|TWICE) A WEEK`)
+	if re.MatchString(line) {
+		freq := re.FindStringSubmatch(line)[1]
+		freqEn := re.FindStringSubmatch(line)[2]
+
+		if isPrn {
+			// prn = true
+			if freq == "1" || freqEn == "ONCE" {
 				return 0, "1 fois par semaine PRN"
 			} else if freq == "2" {
 				return 0, "2 fois par semaine PRN"
@@ -244,7 +442,7 @@ func MapFrequency(line string) (int, string) {
 			}
 		} else {
 			// prn = false
-			if freq == "1" {
+			if freq == "1" || freqEn == "ONCE" {
 				return 0, "1 fois par semaine"
 			} else if freq == "2" {
 				return 0, "2 fois par semaine"
@@ -254,22 +452,71 @@ func MapFrequency(line string) (int, string) {
 		}
 	}
 
-	re = regexp.MustCompile(`(?i)^(?:[^M]+|M(?:AX(?:IMUM|\.))) [0-9]+ (COMPRIMES?) PAR JOUR`)
-	if re.MatchString(line) {
+	// PAR JOUR (mais pas MAXIMUM # COMPRIMES PAR JOUR)
+	re = regexp.MustCompile(`[0-9]+ (COMPRIMES?|CAPSULES?) PAR JOUR`)
+	matches := re.FindAllString(line, -1)
+
+	var filteredMatches []string
+	for _, match := range matches {
+		if !strings.Contains(line[:strings.Index(line, match)], "MAXIMUM") {
+			filteredMatches = append(filteredMatches, match)
+		}
+	}
+	if filteredMatches != nil {
 		if isPrn {
 			return 0, "1 fois par jour PRN"
 		} else {
-			return 0, "1 fois par jour"
+			if strings.Contains(line, "30 MINUTES AVANT LE COUCHER") {
+				return 0, "1 fois par jour au coucher PRN"
+			} else if regexp.MustCompile(`(AU|AVEC LE) DEJEUNER`).MatchString(line) {
+				return 0, "1 fois par jour au déjeuner"
+			}
+		}
+
+		return 0, "1 fois par jour"
+	}
+
+	// PAR SEMAINE (mais pas MAXIMUM # COMPRIMES PAR SEMAINE)
+	re = regexp.MustCompile(`[0-9]+ (COMPRIMES?|CAPSULES?) PAR SEMAINE`)
+	matches = re.FindAllString(line, -1)
+
+	filteredMatches = nil
+	for _, match := range matches {
+		if !strings.Contains(line[:strings.Index(line, match)], "MAXIMUM") {
+			filteredMatches = append(filteredMatches, match)
+		}
+	}
+	if filteredMatches != nil {
+		return 0, "1 fois par semaine"
+	}
+
+	if strings.Contains(line, "LE MATIN") {
+		if isPrn {
+			return 0, "1 fois par jour le matin PRN"
+		} else {
+			return 0, "1 fois par jour le matin"
 		}
 	}
 
-	re = regexp.MustCompile(`UNE SEULE DOSE`)
+	if regexp.MustCompile(`(30 MINUTES|1/2 HEURE) AVANT LE COUCHER`).MatchString(line) {
+		if isPrn {
+			return 0, "1 fois par jour au coucher PRN"
+		} else {
+			return 0, "1 fois par jour au coucher"
+		}
+	}
+
+	re = regexp.MustCompile(`UNE SEULE DOSE|DOSE UNIQUE|UNE SEULE PRISE|IMMEDIATEMENT`)
 	if re.MatchString(line) {
 		if isPrn {
 			return 0, "1 fois PRN"
 		} else {
 			return 0, "1 fois"
 		}
+	}
+
+	if regexp.MustCompile(`GARDER 24 HEURES,? RETIRER .* CHANGER`).MatchString(line) {
+		return 0, "1 fois par jour"
 	}
 
 	return 0, ""
@@ -302,4 +549,16 @@ func RemoveFraction(text string) string {
 	}
 
 	return text
+}
+
+func isComplexDosage(line string) bool {
+	if regexp.MustCompile(`PUIS ([0-9]+) (?:COMPRIMES?|CAPSULES?)`).MatchString(line) {
+		return true
+	}
+
+	if regexp.MustCompile(`MAINTENANT.*CHAQUE SELLE`).MatchString(line) {
+		return true
+	}
+
+	return false
 }
--- main_test.go
+++ main_test.go
@@ -85,6 +85,46 @@ func TestMapDose(t *testing.T) {
 			expectedDose:     "1",
 			expectedDoseUnit: "comprimé",
 		},
+		{
+			input:            "PRENDRE 2 COMPRIMES LE 1ER JOUR, PUIS 1 COMPRIME 1 FOIS PAR JOUR AUX 24 HEURES DU 2IÈME AU 5IÈME JOUR",
+			expectedDose:     "",
+			expectedDoseUnit: "",
+		},
+		{
+			input:            "VAPORISEZ 2 FOIS DANS LES NARINES LE MATIN - REGULIEREMENT (ALLERGIE)",
+			expectedDose:     "2",
+			expectedDoseUnit: "vaporisation",
+		},
+		{
+			input:            "1 GOUTTE DANS L'OEIL AFFECTE 4 FOIS PAR JOUR POUR 7 JOURS",
+			expectedDose:     "1",
+			expectedDoseUnit: "goutte",
+		},
+		{
+			input:            "APPLIQUER 2G. SUR LES ZONES DOULOUREUSES",
+			expectedDose:     "2",
+			expectedDoseUnit: "g",
+		},
+		{
+			input:            "PRENEZ LA CAPSULE EN MANGEANT - DOSE UNIQUE (INFECTION)",
+			expectedDose:     "1",
+			expectedDoseUnit: "capsule",
+		},
+		{
+			input:            "VAPORISEZ DANS LA BOUCHE AUX 5 MINUTES SI BESOIN",
+			expectedDose:     "1",
+			expectedDoseUnit: "vaporisation",
+		},
+		{
+			input:            "INHALER LE CONTENU D'UNE CAPSULE 1 FOIS PAR JOUR LE MATIN",
+			expectedDose:     "1",
+			expectedDoseUnit: "capsule",
+		},
+		{
+			input:            "PRENEZ 2 COMPRIMES MAINTENANT ET 1 COMPRIME APRÈS CHAQUE SELLE LIQUIDE-MAX 8/JR (DIARRHÉE)",
+			expectedDose:     "",
+			expectedDoseUnit: "",
+		},
 	}
 
 	for _, tc := range testCases {
@@ -168,8 +208,8 @@ func TestMapFrequency(t *testing.T) {
 			expected: "2 fois par jour",
 		},
 		{
-			input:    "PRENDRE 1 A 2 COMPRIMES AUX 4 A 6 HEURES SI BESOIN (MAXIMUM 8 COMPRIMES PAR JOUR)",
-			expected: "",
+			input:    "PRENDRE 1 À 2 COMPRIMES AUX 4 A 6  HEURES SI BESOIN (MAXIMUM 8 COMPRIMES PAR JOUR)",
+			expected: "q4h PRN",
 		},
 		{
 			input:    "PRENDRE 1 COMPRIME PAR JOUR",
@@ -179,11 +219,115 @@ func TestMapFrequency(t *testing.T) {
 			input:    "PRENDRE 1 COMPRIME PAR JOUR MAXIMUM 10 COMPRIME PAR JOUR",
 			expected: "1 fois par jour",
 		},
+		{
+			input:    "PRENDRE 1 COMPRIME 1 FOIS PAR JOUR AVANT LE DEJEUNER",
+			expected: "1 fois par jour avant le déjeuner",
+		},
+		{
+			input:    "PRENEZ 1 COMPRIME 30 MINUTES AVANT LE COUCHER - AU BESOIN (INSOMNIE)",
+			expected: "1 fois par jour au coucher PRN",
+		},
+		{
+			input:    "PRENEZ 1 COMPRIME PAR JOUR SANS ARRET",
+			expected: "1 fois par jour",
+		},
+		{
+			input:    "PRENEZ 1 COMPRIME PAR JOUR AVEC LE DEJEUNER - RÉGULIÈREMENT (PRESSION)",
+			expected: "1 fois par jour au déjeuner",
+		},
+		{
+			input:    "PRENDRE 2 COMPRIMES LE 1ER JOUR, PUIS 1 COMPRIME 1 FOIS PAR JOUR AUX 24 HEURES DU 2IÈME AU 5IÈME JOUR",
+			expected: "",
+		},
+		{
+			input:    "VAPORISEZ 2 FOIS DANS LES NARINES LE MATIN - REGULIEREMENT (ALLERGIE)",
+			expected: "1 fois par jour le matin",
+		},
+		{
+			input:    "PRENDRE 1 COMPRIME 1 FOIS PAR JOUR 1/2 HEURE AVANT COUCHER",
+			expected: "1 fois par jour au coucher",
+		},
+		{
+			input:    "PRENEZ 1 COMPRIME AUX 4 HEURES - AU BESOIN (DOULEUR)",
+			expected: "q4h PRN",
+		},
+		{
+			input:    "PRENEZ 2 COMPRIMES AUX 6 HEURES - REGULIEREMENT (DOULEUR - FIEVRE)",
+			expected: "q6h",
+		},
+		{
+			input:    "PRENEZ 1 COMPRIME AUX 4 A 6 HEURES - AU BESOIN (DOULEUR)",
+			expected: "q4h PRN",
+		},
+		{
+			input:    "PRENDRE 1 COMPRIME PAR SEMAINE AVEC 120 ML D'EAU, LE MATIN, AU MOINS 30 MINUTES AVANT NOURRITURE OU AUTRE MEDICAMENT",
+			expected: "1 fois par semaine",
+		},
+		{
+			input:    "PRENDRE 1 CAPSULE UNE FOIS PAR JOUR, A LA MEME HEURE CHAQUE JOUR",
+			expected: "1 fois par jour",
+		},
+		{
+			input:    "TAKE 1 TABLET ONCE DAILY",
+			expected: "1 fois par jour",
+		},
+		{
+			input:    "TAKE 1 TABLET ONCE DAILY AT BEDTIME",
+			expected: "1 fois par jour au coucher",
+		},
+		{
+			input:    "TAKE 1 TABLET ONCE DAILY IN THE MORNING",
+			expected: "1 fois par jour le matin",
+		},
+		{
+			input:    "TAKE 1 TABLET DAILY WITH BREAKFAST - REGULARLY (PRESSURE)",
+			expected: "1 fois par jour au déjeuner",
+		},
+		{
+			input:    "TAKE 1 TABLET ONCE A WEEK",
+			expected: "1 fois par semaine",
+		},
+		{
+			input:    "1 VAPORISATION SOUS LA LANGUE AUX 5 MINUTES SI DOULEUR A LA POITRINE. MAX. 3 VAPORISATIONS SI BESOIN.",
+			expected: "q5min PRN",
+		},
+		{
+			input:    "1 COMPRIME TOUTES LES 4 HEURES SI DOULEURS",
+			expected: "q4h PRN",
+		},
+		{
+			input:    "COLLER UN TIMBRE, GARDER 24 HEURES, RETIRER ET CHANGER. POURSUIVRE PENDANT 6 SEMAINES ET PASSER A L'ETAPE 2",
+			expected: "1 fois par jour",
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run("TestMapFrequency", func(t *testing.T) {
 			_, actual := MapFrequency(tc.input)
+			if actual != tc.expected {
+				t.Errorf("I: %v\nE: %v\nA: %v", tc.input, tc.expected, actual)
+				return
+			}
+		})
+	}
+}
+
+func TestMapRoute(t *testing.T) {
+	testCases := []struct {
+		input    string
+		doseUnit string
+		expected string
+	}{
+		{
+			input:    "INHALER LE CONTENU D'UNE CAPSULE 1 FOIS PAR JOUR LE MATIN",
+			doseUnit: "capsule",
+			expected: "inhalation",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("TestMapRoute", func(t *testing.T) {
+			actual := MapRoute(tc.input, Dosage{DoseUnit: tc.doseUnit})
 			if actual != tc.expected {
 				t.Errorf("I: %v\nE: %v\nA: %v", tc.input, tc.expected, actual)
 				return
